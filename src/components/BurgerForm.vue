aaf519969c1d259da1b7f009a43ea50873de75bb
JordanoTI <jordanoffice97@gmail.com>
2021-12-17 22:21:28 -0300

    Inserindo dados no banco

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Componente de mensagem</p>
     <div>
-      <form id="burger-form">
+      <form id="burger-form" @submit="createBurger">
         <div class="input-container">
           <label for="nome">Nome do cliente:</label>
           <input
@@ -32,7 +32,9 @@
           </select>
         </div>
         <div id="opcionais-container" class="input-container">
-          <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
+          <label id="opcionais-title" for="opcionais"
+            >Selecione os opcionais:</label
+          >
           <div
             class="checkbox-container"
             v-for="opcional in opcionaisdata"
@@ -79,6 +81,29 @@ export default {
       this.paes = data.paes;
       this.carnes = data.carnes;
       this.opcionaisdata = data.opcionais;
+    },
+    async createBurger(e) {
+      e.preventDefault();
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado",
+      };
+      const dataJson = JSON.stringify(data);
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      });
+      const res = await req.json();
+      // colocar uma msg de sistema
+      // limpar os campos
+      this.nome = "";
+      this.carne = "";
+      this.pao = "";
+      this.opcionais = "";
     },
   },
   mounted() {
